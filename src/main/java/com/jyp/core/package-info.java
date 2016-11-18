7f6d542667f5e86385ac9a833d16f554367318ea
JinuxDream <422053690@qq.com>
2016-11-18 17:00:53 +0800

    spring mv c 环境搭建基本

---
+++ src/main/java/com/jyp/core/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 框架核心
+ * Created by jyp on 2016/11/18.
+ */
+package com.jyp.core;
---
+++ src/main/java/com/jyp/dao/UserInfoMapper.java
@@ -0,0 +1,8 @@
+package com.jyp.dao;
+
+/**
+ * Created by jyp on 2016/11/18.
+ */
+
+public interface UserInfoMapper {
+}
